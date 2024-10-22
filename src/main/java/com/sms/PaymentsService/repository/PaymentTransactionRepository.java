b1971f925f3ae52df6c657e932b18bc0e1d699f8
Vijay <delavarasantamil2001@gmail.com>
2024-10-22 09:02:21 +0530

    Payment Transaction

--- src/main/java/com/sms/PaymentsService/repository/PaymentTransactionRepository.java
+++ src/main/java/com/sms/PaymentsService/repository/PaymentTransactionRepository.java
@@ -5,8 +5,11 @@ import com.sms.PaymentsService.entity.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, Integer> {
 
+    List<PaymentTransaction> findBySubscriptionId(Integer subscriptionId);
 
 }
